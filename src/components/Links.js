c57141568577ce340cc5987ff59f0b39ad00ee92
David Wenzel <dwenzel95@gmail.com>
2022-05-14 20:14:19 -0500

    commit tree ac0bccde3b390f285460427d221c7e96bdaa8b25

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// const name = user && user.personalInfo ? user.personalInfo.name : null
+
+function Links(props) {
+    return( 
+        <div>
+        <h3>Links</h3>
+        <a>props.</a>
+        </div>
+    )
+}
+
+export default Links
